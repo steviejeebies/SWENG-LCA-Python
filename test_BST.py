3a5abefd312660aae276d2cd836636d06ef37a60
Stephen Rowe <rowes@tcd.ie>
2020-10-17 01:52:14 +0100

    testing done, code coverage is 100%

--- test_BST.py
+++ test_BST.py
@@ -1,3 +1,5 @@
+# 100% test coverage
+
 from unittest import TestCase
 from BST import *
 
@@ -8,6 +10,7 @@ class TestBST(TestCase):
         string_bst.add_node("string a")
         string_bst.add_node("string b")
         string_bst.add_node("string c")
+        string_bst.add_node("string c")     # adding repeat value
 
         self.assertEqual("string a", string_bst.find_node("string a").key)
         self.assertEqual("string b", string_bst.find_node("string b").key)
@@ -15,15 +18,30 @@ class TestBST(TestCase):
         self.assertIsNone(string_bst.find_node("string d"))
 
     def test_add_node_int(self):
-        string_bst = BST()
-        string_bst.add_node(1)
-        string_bst.add_node(2)
-        string_bst.add_node(3)
+        int_bst = BST()
+        int_bst.add_node(1)
+        int_bst.add_node(2)
+        int_bst.add_node(3)
 
-        self.assertEqual(1, string_bst.find_node(1).key)
-        self.assertEqual(2, string_bst.find_node(2).key)
-        self.assertEqual(3, string_bst.find_node(3).key)
-        self.assertIsNone(string_bst.find_node(4))
+        self.assertEqual(1, int_bst.find_node(1).key)
+        self.assertEqual(2, int_bst.find_node(2).key)
+        self.assertEqual(3, int_bst.find_node(3).key)
+        self.assertIsNone(int_bst.find_node(4))
+
+    def test_find_node(self):
+        int_bst = BST()
+        # testing trying to find a node when no nodes are on BST
+        self.assertIsNone(int_bst.find_node(1))
+
+        # adding the node of int 1 to the tree, then attempting to find this node
+        int_bst.add_node(1)
+        int_bst.add_node(5)
+        int_bst.add_node(3)
+        self.assertEqual(1, int_bst.find_node(1).key)
+        self.assertEqual(5, int_bst.find_node(5).key)
+        self.assertEqual(3, int_bst.find_node(3).key)
+        self.assertIsNone(int_bst.find_node(2))
+        self.assertIsNone(int_bst.find_node(4))
 
     def test_find_lca(self):
         our_tree = BST()
@@ -33,7 +51,7 @@ class TestBST(TestCase):
             our_tree.add_node(x)
 
         # LCA of 12 4 is 9
-        self.assertEqual(9, our_tree.find_lca(12, 4))
+        self.assertEqual(9, our_tree.find_lca(12, 4), "test")
         # LCA of 7 4 is 4
         self.assertEqual(4, our_tree.find_lca(7, 4))
         # LCA of 14 22 is 15
@@ -46,4 +64,3 @@ class TestBST(TestCase):
         self.assertEqual(9, our_tree.find_lca(7, 13))
         # LCA of 30 99 (99 not on tree) is 30
         self.assertEqual(30, our_tree.find_lca(30, 99))
-
